507621b65cce7dacfd92838d536c88fd82b23c20
Oggy1923 <oguzhan@heelocoupons.com>
2018-11-04 18:31:51 -0500

    Made a simple custom Request Queue that requests a String from the server

--- app/src/main/java/com/gonel/oguzhan/android_volley2/MainActivity.java
+++ app/src/main/java/com/gonel/oguzhan/android_volley2/MainActivity.java
@@ -6,10 +6,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -22,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
 
     RequestQueue requestQueue;
+    Cache cache;
+    Network network;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +36,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.txtView);
+
+        // Since we are implementing our own Request Queue, we have to make our own cache
+        cache = new DiskBasedCache(getCacheDir(), 1024*1024);
+
+        // The Network
+        network = new BasicNetwork(new HurlStack());
+
+        // Initialize the Request Queue, takes the cache and network as parameters
+        requestQueue = new RequestQueue(cache, network);
+
+        // Start the Request Queue
+        requestQueue.start();
     }
 
     public void serverButtonFunction(View view){
 
         Log.d("serverButton", "Server Button has been clicked");
 
-        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        // The below Request Queue is no longer used as I created my own Request Queue
+        // requestQueue = Volley.newRequestQueue(MainActivity.this);
 
         // I make a String Request to get from the server
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
